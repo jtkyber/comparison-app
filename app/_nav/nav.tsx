8a876dbad53e449727c1d7a91a3a8aafd571cdb8
Joey Kyber <joeykyber@gmail.com>
2025-10-04 02:17:17 -0400

    Began basic app structure

---
+++ app/_nav/nav.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './nav.module.css';
+
+const Nav = () => {
+	return <div className={styles.nav_container}></div>;
+};
+
+export default Nav;
---
+++ app/_table_manager/table_manager.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './table_manager.module.css';
+
+const TableManager = () => {
+	return <div className={styles.table_manager_container}></div>;
+};
+
+export default TableManager;
