2d4ecfbdd2b68b80303d6db9652761dfe6d9ba3a
Joey Kyber <joeykyber@gmail.com>
2025-11-08 03:21:37 -0500

    Table style tweaks

--- src/components/table_display/table_display.tsx
+++ src/components/table_display/table_display.tsx
@@ -191,98 +191,102 @@ const TableDisplay = () => {
 		calculateFinalRatings();
 	}, [attributes, entries]);
 
-	return attributes.length || entries.length ? (
+	return (
 		<div className={styles.table_display_container}>
-			<table
-				className={`${styles.table} ${fitColMin ? styles.fit_cell_min : null} ${
-					colorCellsByRating ? styles.colored : null
-				}`}>
-				<thead>
-					<tr>
-						<th>&nbsp;</th>
-						{attributes
-							.filter(attr => !attr.hidden)
-							.map(attr => (
-								<th
-									className={`${styles.attribute_name} ${
-										display.highlightedAttribute === attr.id ? styles.highlighted : null
-									}`}
-									key={attr.id}>
-									{attr?.name}
-								</th>
-							))}
-						<th>Score</th>
-					</tr>
-				</thead>
-				<tbody>
-					{entries
-						.filter(entry => !entry.hidden)
-						.map(entry => {
-							const { id: entryID, name: entryName } = entry || {};
+			{attributes.length || entries.length ? (
+				<>
+					<table
+						className={`${styles.table} ${fitColMin ? styles.fit_cell_min : null} ${
+							colorCellsByRating ? styles.colored : null
+						}`}>
+						<thead>
+							<tr>
+								<th>&nbsp;</th>
+								{attributes
+									.filter(attr => !attr.hidden)
+									.map(attr => (
+										<th
+											className={`${styles.attribute_name} ${
+												display.highlightedAttribute === attr.id ? styles.highlighted : null
+											}`}
+											key={attr.id}>
+											{attr?.name}
+										</th>
+									))}
+								<th>Score</th>
+							</tr>
+						</thead>
+						<tbody>
+							{entries
+								.filter(entry => !entry.hidden)
+								.map(entry => {
+									const { id: entryID, name: entryName } = entry || {};
 
-							return (
-								<tr
-									className={`${styles.table_row} ${
-										display.highlightedEntry === entryID ? styles.highlighted : null
-									}`}
-									key={entryID}>
-									<th>{entryName}</th>
-									{attributes
-										.filter(attr => !attr.hidden)
-										.map(attr => {
-											const { value } = entry.cells[attr.id] || {};
-											const { id: attrID, prefix, suffix, type } = attr || {};
+									return (
+										<tr
+											className={`${styles.table_row} ${
+												display.highlightedEntry === entryID ? styles.highlighted : null
+											}`}
+											key={entryID}>
+											<th>{entryName}</th>
+											{attributes
+												.filter(attr => !attr.hidden)
+												.map(attr => {
+													const { value } = entry.cells[attr.id] || {};
+													const { id: attrID, prefix, suffix, type } = attr || {};
 
-											return (
-												<td
-													className={`${styles.cell} ${
-														display.highlightedAttribute === attrID ? styles.highlighted : null
-													}`}
-													style={{
-														backgroundColor: colorCellsByRating
-															? determineCellColor(entryID, attrID)
-															: 'transparent',
-													}}
-													key={`${entryID}_${attrID}`}
-													onClick={() => handleCellClick(entryID, attrID)}>
-													{prefix}
-													{type === 'link' ? (
-														<a
-															className={`${styles.link} ${ctrlDown ? styles.clickable : null}`}
-															href={value as string}
-															target='_blank'
-															rel='noopener noreferrer'>
-															Link
-														</a>
-													) : type === 'yesNo' ? (
-														value === true ? (
-															'Yes'
-														) : (
-															'No'
-														)
-													) : (
-														value
-													)}
-													{suffix}
-												</td>
-											);
-										})}
-									<td
-										style={{
-											backgroundColor: colorCellsByRating
-												? ratingToColor(display.entryRatings[entry.id]?.rating)
-												: 'transparent',
-										}}
-										className={styles.rating_cell}>
-										{display.entryRatings[entry.id]?.rating || ''}
-									</td>
-								</tr>
-							);
-						})}
-				</tbody>
-			</table>
+													return (
+														<td
+															className={`${styles.cell} ${
+																display.highlightedAttribute === attrID ? styles.highlighted : null
+															}`}
+															style={{
+																backgroundColor: colorCellsByRating
+																	? determineCellColor(entryID, attrID)
+																	: 'transparent',
+															}}
+															key={`${entryID}_${attrID}`}
+															onClick={() => handleCellClick(entryID, attrID)}>
+															{prefix}
+															{type === 'link' ? (
+																<a
+																	className={`${styles.link} ${ctrlDown ? styles.clickable : null}`}
+																	href={value as string}
+																	target='_blank'
+																	rel='noopener noreferrer'>
+																	Link
+																</a>
+															) : type === 'yesNo' ? (
+																value === true ? (
+																	'Yes'
+																) : (
+																	'No'
+																)
+															) : (
+																value
+															)}
+															{suffix}
+														</td>
+													);
+												})}
+											<td
+												style={{
+													backgroundColor: colorCellsByRating
+														? ratingToColor(display.entryRatings[entry.id]?.rating)
+														: 'transparent',
+												}}
+												className={styles.rating_cell}>
+												{display.entryRatings[entry.id]?.rating || ''}
+											</td>
+										</tr>
+									);
+								})}
+						</tbody>
+					</table>
+				</>
+			) : null}
 		</div>
-	) : null;
+	);
 };
 
 export default TableDisplay;
